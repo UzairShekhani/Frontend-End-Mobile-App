38e790995e4aa68f3f58cba8c6cb335aeeb00a1a
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-18 16:38:15 +0500

    UI UPDATED

---
+++ app.js
@@ -0,0 +1,58 @@
+var users = [
+    {
+        profilePic:"",
+        displayPic: "",
+        pendingMessages: 4,
+        location: "karachi , Pakistan",
+        name: "Uzair",
+        age: 18,
+        interests: ["Music" , "Painting"],
+        bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, et aliquid, nam debitis neque quas voluptatibus hic",
+        isFriend: null
+    },
+    {
+        profilePic:"https://images.unsplash.com/photo-1528892952291-009c663ce843?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+        displayPic: "",
+        pendingMessages: 4,
+        location: "karachi , Pakistan",
+        name: "Adil Ahmed",
+        age: 33,
+        interests: ["Music" , "Painting"],
+        bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, et aliquid, nam debitis neque quas voluptatibus hic",
+        isFriend: null
+    },
+    {
+        profilePic:"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+        displayPic: "",
+        pendingMessages: 4,
+        location: "karachi , Pakistan",
+        name: "Sadia",
+        age: 28,
+        interests: ["Music" , "Painting"],
+        bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, et aliquid, nam debitis neque quas voluptatibus hic",
+        isFriend: null
+    },
+    {
+        profilePic:"https://images.unsplash.com/photo-1519895609939-d2a6491c1196?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+        displayPic: "",
+        pendingMessages: 4,
+        location: "karachi , Pakistan",
+        name: "Rabia",
+        age: 5,
+        interests: ["Music" , "Painting"],
+        bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, et aliquid, nam debitis neque quas voluptatibus hic",
+        isFriend: null
+    },
+    {
+        profilePic:"https://plus.unsplash.com/premium_photo-1683140621573-233422bfc7f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+        displayPic: "",
+        pendingMessages: 4,
+        location: "karachi , Pakistan",
+        name: "Anabia",
+        age: 4,
+        interests: ["Music" , "Painting"],
+        bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, et aliquid, nam debitis neque quas voluptatibus hic",
+        isFriend: null
+    },
+
+]
